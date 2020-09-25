4db7959b09dfc299ce0bc768718248a88a51d943
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-09-25 18:10:09 +0200

    Add initial unit tests (#1)

--- src/PrintMembersGenerator/ISymbolExtensions.cs
+++ src/PrintMembersGenerator/ISymbolExtensions.cs
@@ -4,6 +4,18 @@ namespace PrintMembersGenerator
 {
     internal static class ISymbolExtensions
     {
+        /// <summary>
+        /// Determines whether the compiler includes the symbol in PrintMembers or not.
+        /// </summary>
+        /// <remarks>
+        /// The compiler includes the symbol if all the following is true:
+        /// 1. Symbol is public.
+        /// 2. Symbol is not static or constant.
+        /// 3. Symbol is either a field or a property.
+        /// 3. Symbol is not an indexer.
+        /// 4. Symbol is not an override.
+        /// 5. Symbol is not a set-only/init-only property.
+        /// </remarks>
         public static bool IsIncludedByDefault(this ISymbol symbol)
         {
             // This method is an exact copy from Roslyn's SynthesizedRecordPrintMembers code:
---
+++ src/PrintMembersGeneratorTests/PrintMembersGeneratorTest.cs
@@ -0,0 +1,130 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using PrintMembersGenerator;
+using Xunit;
+
+namespace PrintMemberGeneratorTests
+{
+    public class GeneratorTests
+    {
+        [Fact]
+        public void SimpleGeneratorTest()
+        {
+            string source = @"
+namespace MyCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+";
+            Compilation comp = CreateCompilation(source);
+            Compilation newComp = RunGenerators(comp, out ImmutableArray<Diagnostic> generatorDiags, new PrintMembersSourceGenerator());
+            IEnumerable<SyntaxTree> generatedTrees = newComp.RemoveSyntaxTrees(comp.SyntaxTrees).SyntaxTrees;
+
+            Assert.Single(generatedTrees);
+            Assert.Empty(generatorDiags);
+            Assert.Empty(newComp.GetDiagnostics());
+        }
+
+        [Fact]
+        public void NonPartialRecord_ShouldBeIgnored()
+        {
+            string source = @"
+using SourceGenerator;
+
+namespace MyCode
+{
+    public record R
+    {
+        [PrintMembers]
+        public const int I = 13;
+
+        [PrintMembers(ShouldInclude = true)]
+        public const string  S = ""Youssef"";
+
+        [PrintMembers(ShouldInclude = false)]
+        public string X { get; set; }
+    }
+}
+";
+            Compilation comp = CreateCompilation(source, OutputKind.DynamicallyLinkedLibrary);
+            Compilation newComp = RunGenerators(comp, out ImmutableArray<Diagnostic> generatorDiags, new PrintMembersSourceGenerator());
+            IEnumerable<SyntaxTree> generatedTrees = newComp.RemoveSyntaxTrees(comp.SyntaxTrees).SyntaxTrees;
+
+            Assert.Single(generatedTrees);
+            Assert.Empty(generatorDiags);
+            Assert.Empty(newComp.GetDiagnostics());
+        }
+
+        [Fact]
+        public void PartialRecord_01()
+        {
+            string source = @"
+using SourceGenerator;
+
+namespace MyCode
+{
+    public partial record R
+    {
+        [PrintMembers]
+        public const int I = 13;
+
+        [PrintMembers(ShouldInclude = true)]
+        public const string  S = ""Youssef"";
+
+        [PrintMembers(ShouldInclude = false)]
+        public string X { get; set; }
+    }
+}
+";
+            Compilation comp = CreateCompilation(source, OutputKind.DynamicallyLinkedLibrary);
+            Compilation newComp = RunGenerators(comp, out ImmutableArray<Diagnostic> generatorDiags, new PrintMembersSourceGenerator());
+            IEnumerable<SyntaxTree> generatedTrees = newComp.RemoveSyntaxTrees(comp.SyntaxTrees).SyntaxTrees;
+
+            Assert.Equal(expected: 2, actual: generatedTrees.Count()); // Attribute tree and record's tree.
+            Assert.Equal(expected: generatedTrees.Last().GetText().ToString(), actual:
+@"namespace MyCode
+{
+    public partial record R
+    {
+        protected virtual bool PrintMembers(System.Text.StringBuilder builder)
+        {
+            builder.Append(""I"");
+            builder.Append("" = "");
+            builder.Append(I.ToString());
+            builder.Append("", "");
+            builder.Append(""S"");
+            builder.Append("" = "");
+            builder.Append((object)S);
+            return true;
+        }
+    }
+}");
+            Assert.Empty(generatorDiags);
+            Assert.Empty(newComp.GetDiagnostics());
+        }
+
+        private static Compilation CreateCompilation(string source, OutputKind outputKind = OutputKind.ConsoleApplication)
+            => CSharpCompilation.Create("compilation",
+                new[] { CSharpSyntaxTree.ParseText(source, new CSharpParseOptions(LanguageVersion.Preview)) },
+                new[] { MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location) },
+                new CSharpCompilationOptions(outputKind));
+
+        private static GeneratorDriver CreateDriver(Compilation c, params ISourceGenerator[] generators)
+            => CSharpGeneratorDriver.Create(generators, parseOptions: (CSharpParseOptions)c.SyntaxTrees.First().Options);
+
+        private static Compilation RunGenerators(Compilation c, out ImmutableArray<Diagnostic> diagnostics, params ISourceGenerator[] generators)
+        {
+            CreateDriver(c, generators).RunGeneratorsAndUpdateCompilation(c, out var d, out diagnostics);
+            return d;
+        }
+    }
+}
